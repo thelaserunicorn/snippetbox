4385c889059514e4b7844304963262292b70a553
Rashid <rashideram2@gmail.com>
2025-05-26 22:35:41 +0600

    added snippet model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,53 @@
 package main
+
 import (
-  "flag"
-  "log/slog"
-  "net/http"
-  "os"
+    "database/sql"
+    "flag"
+    "log/slog"
+    "net/http"
+    "os"
+
+    // Import the models package that we just created. You need to prefix this with
+    // whatever module path you set up back in chapter 02.01 (Project Setup and Creating
+    // a Module) so that the import statement looks like this:
+    // "{your-module-path}/internal/models". If you can't remember what module path you 
+    // used, you can find it at the top of the go.mod file.
+    "snippetbox.alexedwards.net/internal/models" 
+
+    _ "github.com/go-sql-driver/mysql"
 )
 
+// Add a snippets field to the application struct. This will allow us to
+// use the SnippetModel type in our handlers.
 type application struct {
-  logger *slog.Logger
+    logger   *slog.Logger
+    snippets *models.SnippetModel
 }
 
 func main() {
-  logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-  addr := flag.String("addr", ":4000", "HTTP Network Address")
-  flag.Parse()
-  
-  app := &application{
-    logger: logger,
-  }
+    addr := flag.String("addr", ":4000", "HTTP network address")
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+    flag.Parse()
 
+    logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-  logger.Info("starting server", "addr", *addr)
-  err := http.ListenAndServe(*addr, app.routes())
-  logger.Error(err.Error())
-  os.Exit(1)
+    db, err := openDB(*dsn)
+    if err != nil {
+        logger.Error(err.Error())
+        os.Exit(1)
+    }
+    defer db.Close()
+
+    // Initialize a models.SnippetModel instance containing the connection pool
+    // and add it to the application dependencies.
+    app := &application{
+        logger:   logger,
+        snippets: &models.SnippetModel{DB: db},
+    }
+
+    logger.Info("starting server", "addr", *addr)
+
+    err = http.ListenAndServe(*addr, app.routes())
+    logger.Error(err.Error())
+    os.Exit(1)
 }
