640b200f52d04406735280fd0706d10133887d95
Rashid <rashideram2@gmail.com>
2025-05-26 13:40:34 +0600

    added routes

---
+++ cmd/web/main.go
@@ -0,0 +1,23 @@
+package main
+import (
+  "flag"
+  "log"
+  "net/http"
+)
+
+
+func main() {
+  addr := flag.String("addr", ":4000", "HTTP Network Address")
+  flag.Parse()
+  mux := http.NewServeMux()
+  fileServer := http.FileServer(http.Dir("./ui/static/"))
+  mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+  mux.HandleFunc("GET /{$}", home)
+  mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+  mux.HandleFunc("GET /snippet/create", snippetCreate)
+  mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+  log.Printf("Server Starting On Port %s", *addr)
+  err := http.ListenAndServe(*addr, mux)
+  log.Fatal(err)
+}
